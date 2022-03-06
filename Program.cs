f6888dbaf22090433d9465a5679ed4456eac4711
Adam Wieliczko <niszczyciel.90@wp.pl>
2022-03-06 16:06:40 +0100

    Test branch commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(1);
+Console.WriteLine(2);
+Console.WriteLine(3);
+Console.WriteLine(4);
+Console.WriteLine(5);

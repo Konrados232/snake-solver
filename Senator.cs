0d00aac87b403facde9d5ad3b84bd06ad6a3b8d6
Konrad Nowak <shaderpl@gmail.com>
2022-03-06 17:03:18 +0100

    Add Senator class

---
+++ Senator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colab_proj
+{
+    internal class Senator
+    {
+
+        public string Name { get; private set; }
+
+        public Senator()
+        {
+            Name = "Armstrong";
+        }
+
+        public Senator(string name)
+        {
+            Name = name;
+        }
+
+    }
+}

a3c85811d02b8193a2e87a4c6a944a47bb40d4a1
Konrad Nowak <shaderpl@gmail.com>
2022-03-06 16:09:08 +0100

    Test branch commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+List<int> intList = new();
+intList.Add(1);
+intList.Add(2);
+intList.Add(3);
+intList.Add(4);
+intList.Add(5);
+
+
